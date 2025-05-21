18d8f8a622d54f9ce57112259c5d6af349978c39
smilekevin <kevin7261@gmail.com>
2025-05-21 13:42:49 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// ✅ 若你的 GitHub repo 是 my-vue-project，請填 '/my-vue-project/'
+// ✅ 若你的 repo 是 username.github.io（部署到主頁），請填 '/'。
+export default defineConfig({
+  base: '/long-term-care-web/',
+  plugins: [vue()]
+})``
